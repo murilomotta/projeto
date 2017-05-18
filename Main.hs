901ec0f3273b9bc29ae324b35a656e16f72413d1
murilomotta <murilo.gomes.motta@gmail.com>
2017-05-18 03:15:52 +0000

    Main - conexão com o banco de dados

---
+++ Main.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings          #-}
+import Application () -- for YesodDispatch instance
+import Foundation
+import Yesod
+import Control.Monad.Logger (runStdoutLoggingT)
+import Database.Persist.Postgresql
+import Data.Text
+import Data.Text.Encoding
+
+connStr :: Text
+connStr = "dbname=dd9en8l5q4hh2a host=ec2-107-21-219-201.compute-1.amazonaws.com user=kpuwtbqndoeyqb password=aCROh525uugAWF1l7kahlNN3E0 port=5432"
+
+-- codenvy 3000
+-- warp 8080 App
+main :: IO ()
+main = runStdoutLoggingT $ withPostgresqlPool (encodeUtf8 connStr) 10 $ \pool -> liftIO $ do
+    runSqlPersistMPool (runMigration migrateAll) pool 
+    warp 3000 (App pool)
+
